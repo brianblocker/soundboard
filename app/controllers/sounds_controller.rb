53a5e7034e8c3f13510dd178ce9d2bcc914ec6d1
Brian Blocker <blocker.brian@gmail.com>
2013-10-19 16:11:28 -0700

    added all sounds and ios stuff

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -1,14 +1,21 @@
 class SoundsController < ApplicationController
+  
   def index
     @sounds = [
-      {value: 'race-start', label: 'Start Race'}
+      {value: 'race-start', label: 'Start Race'},
+      {value: 'passby-1', label: 'Pass By 1'},
+      {value: 'passby-2', label: 'Pass By 2'},
+      {value: 'passby-3', label: 'Pass By 3'},
+      {value: 'siren-1', label: 'Siren 1'},
+      {value: 'siren-2', label: 'Siren 2'}
     ]
   end
 
   def sound
+    sounds_path = File.expand_path(File.dirname(__FILE__) + '/../../audio')
     puts params
 
-    system("afplay ~/Desktop/audio/#{params[:sound]}.mp3")
+    system("afplay #{sounds_path}/#{params[:sound]}.mp3")
 
     render nothing: true
   end
